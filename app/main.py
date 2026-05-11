3f490fde7e518249c268621f4dd93916f6566f53
Javiervieytes <163855235+Javiervieytes@users.noreply.github.com>
2026-05-11 02:00:39 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ load_dotenv()
 app = FastAPI(
     title="MapacheSecure API",
     description="Backend para el sistema de autorregulación digital MapacheSecure",
-    version="1.0.6"
+    version="1.0.7"
 )
 
 # Routers
@@ -24,5 +24,5 @@ app.include_router(bloqueos.router)
 def root():
     return {
         "mensaje": "🦝 MapacheSecure API funcionando correctamente",
-        "version": "1.0.6"
+        "version": "1.0.7"
     }
