d0ce8044fd00ae164203c10eafebb8f292b4323a
Javiervieytes <163855235+Javiervieytes@users.noreply.github.com>
2026-05-21 22:16:15 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -24,5 +24,5 @@ app.include_router(canjes.router)
 def root():
     return {
         "mensaje": "🦝 MapacheSecure API funcionando correctamente",
-        "version": "1.1.3"
+        "version": "1.1.4"
     }
